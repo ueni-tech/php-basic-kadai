93b95c9ef6f31456a867ed9d8e0706db2dd052f6
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2023-09-11 14:36:21 +0900

    課題7　提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP基礎編 課題007</title>
+</head>
+<body>
+  <p>
+  <?php
+  $onion = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+  print_r($onion);
+  ?>
+  </p>
+
+</body>
+
+</html>
